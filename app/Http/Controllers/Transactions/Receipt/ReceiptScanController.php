9bb573595eeb617ddccebcc8a48376be9ca02da5
ShodanMaster <akshaykrishnan922@gmail.com>
2025-10-30 10:21:48 +0530

    Setup Receipt Scan

---
+++ app/Http/Controllers/Transactions/Receipt/ReceiptScanController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Transactions\Receipt;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ReceiptScanController extends Controller
+{
+    //
+}
